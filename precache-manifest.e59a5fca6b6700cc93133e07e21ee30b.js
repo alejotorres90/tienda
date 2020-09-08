37a66dd593935d63fc82c18cfb041632287f7b77
Alejo Torres <alejotorres@live.com.ar>
2020-09-08 13:35:48 -0300

    Initial commit

---
+++ precache-manifest.e59a5fca6b6700cc93133e07e21ee30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac94522987ea33578336f358b5badae",
+    "url": "/tienda/index.html"
+  },
+  {
+    "revision": "e1bf88de98f86ba3df09",
+    "url": "/tienda/static/css/main.1a7d3c44.chunk.css"
+  },
+  {
+    "revision": "4db57c8c705f6d103272",
+    "url": "/tienda/static/js/2.cfdc079c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tienda/static/js/2.cfdc079c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bf88de98f86ba3df09",
+    "url": "/tienda/static/js/main.493812da.chunk.js"
+  },
+  {
+    "revision": "2b2c92185fb62961cca9",
+    "url": "/tienda/static/js/runtime-main.7e0eb0d9.js"
+  }
+]);
